55b6182f2ad6f465996079fb9ae9883f06fc3066
Josett Castillo <josettcastillo@gmail.com>
2025-09-25 18:37:59 -0600

    Task 1 resuelta

---
+++ task1.js
@@ -0,0 +1,12 @@
+function js_style() {
+
+    const paragraph = document.getElementById("text");
+    paragraph.style.fontFamily ="Orbitron, 'Courier New', monospace";
+    paragraph.style.fontSize = "24px";
+    paragraph.style.color = "#07a38eff";
+    paragraph.style.fontWeight = "bold";
+    paragraph.style.textAlign = "center";
+    paragraph.style.padding = "10px";
+    paragraph.style.backgroundColor = "#ccde9fff";
+    paragraph.style.borderRadius = "5px";
+}
